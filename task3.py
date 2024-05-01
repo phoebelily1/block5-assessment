014b153386f790c18c07e1f810f904fdf42a38e3
Tanishmadhok <167077560+Tanishmadhok@users.noreply.github.com>
2024-05-01 14:04:59 +0100

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,7 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-
+'''
+        Initializes a node with properties: value, index, and connections.
+        Value represents the node's associated value, index serves as its unique identifier,
+        and connections is a list of indices of nodes connected to it. If no connections are specified during initialization,
+        it defaults to an empty list.
+'''
 
 class Node:
     def __init__(self, value, number, connections=None):
@@ -11,12 +16,20 @@ class Node:
 
 class Network:
     def __init__(self, nodes=None):
+        '''
+        Initializes a network with a list of nodes, initializing an empty list if no nodes are provided.
+        '''
         self.nodes = nodes if nodes is not None else []
 
     def get_mean_degree(self):
         total_connection = sum(len(node.connections) for node in self.nodes)
         mean_degree = total_connection / len(self.nodes)
         return mean_degree
+         '''
+        Calculates the average number of connections each node has in the network.
+        It sums the lengths of connections lists for all nodes and divides by the total number of nodes to find the mean degree.
+        This metric helps assess the network's overall connectivity.
+        '''
 
     def get_mean_clustering(self):
         total_clustering = 0
@@ -37,6 +50,11 @@ class Network:
 
         mean_clustering_coefficient = total_clustering / len(self.nodes)
         return mean_clustering_coefficient
+
+    '''
+        Calculates the average tendency for nodes to cluster together by computing the clustering coefficient for each node.
+        It measures how closely neighbors are connected and averages these coefficients across all nodes to assess clustering in the network.
+   '''
 
     def get_mean_path_length(self):
         total_mean_path_length = 0
@@ -60,3 +78,6 @@ class Network:
                 total_mean_path_length += total_path_length / num_neighbors
 
         return total_mean_path_length / len(self.nodes)
+        '''
+        Computes the average distance between nodes in the network by finding shortest paths between all pairs of nodes.
+        '''
